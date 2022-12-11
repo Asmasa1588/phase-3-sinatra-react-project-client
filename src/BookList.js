f689b469ad93e4d4b7e3cad6dec8e72322aa2933
Asma <as1588ma@gmail.com>
2022-12-10 22:50:52 -0500

    connect frontend with backend

--- src/BookList.js
+++ src/BookList.js
@@ -1,3 +1,14 @@
+import { useEffect, useState } from "react";
+
 export const BookList = () => {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:9292/books")
+      .then((res) => res.json())
+      .then((receivedBooks) => {
+        setBooks(receivedBooks);
+        console.log({ receivedBooks });
+      });
+  }, []);
   return <div>Here we will list books</div>;
 };
