cbf3b3a9198d30e174b3b84395ba333e1237e9ec
Asma <as1588ma@gmail.com>
2022-12-20 13:17:22 -0500

    introduce create book functionality

--- src/BookList.js
+++ src/BookList.js
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom";
 
 export const BookList = () => {
   const [books, setBooks] = useState([]);
+  const [showCreateBookForm, setShowCreateBookForm] = useState(false);
+  const [bookForm, setBookForm] = useState({
+    author: "",
+    title: "",
+    year: "",
+    pages: "",
+  });
   useEffect(() => {
     fetch("http://localhost:9292/books")
       .then((res) => res.json())
@@ -27,6 +34,46 @@ export const BookList = () => {
             </div>
           );
         })}
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setShowCreateBookForm(true);
+          }}
+        >
+          Create a New Book
+        </button>
+        {showCreateBookForm && (
+          <form
+            onChange={(event) => {
+              setBookForm((prev) => {
+                return { ...prev, [event.target.name]: event.target.value };
+              });
+            }}
+            onSubmit={(event) => {
+              event.preventDefault();
+              console.log({ bookForm });
+              fetch("http://localhost:9292/books", {
+                method: "POST",
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(bookForm),
+              });
+            }}
+          >
+            <label>Title</label>
+            <input name="title" value={bookForm.title} />
+            <label>Author</label>
+            <input name="author" value={bookForm.author} />
+            <label>Year</label>
+            <input name="year" value={bookForm.year} />
+            <label>Pages</label>
+            <input name="pages" value={bookForm.pages} />
+            <button type="submit">Submit</button>
+          </form>
+        )}
       </div>
     </div>
   );
