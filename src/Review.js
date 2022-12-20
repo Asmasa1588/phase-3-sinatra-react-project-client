5f1440f42a9deba9af07803dc464a4b9ee8a4392
Asma <as1588ma@gmail.com>
2022-12-20 14:55:18 -0500

    make an edit review UX better

--- src/Review.js
+++ src/Review.js
@@ -12,6 +12,7 @@ export const Review = () => {
         setReview(receivedReview.review);
       });
   }, [reviewId]);
+  console.log({ review });
   return (
     <div>
       <h2>Edit Review</h2>
@@ -21,24 +22,26 @@ export const Review = () => {
           setReview(event.target.value);
         }}
       />
-      <button
-        onClick={() => {
-          fetch(`http://localhost:9292/reviews/${reviewId}`, {
-            method: "PATCH",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ review }),
-          })
-            .then((res) => res.json())
-            .then(() => {
-              //in case we need to do stuff after updating the review
-            });
-        }}
-      >
-        Edit
-      </button>
+      {review !== "" && (
+        <button
+          onClick={() => {
+            fetch(`http://localhost:9292/reviews/${reviewId}`, {
+              method: "PATCH",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ review }),
+            })
+              .then((res) => res.json())
+              .then(() => {
+                setReview("");
+              });
+          }}
+        >
+          Edit
+        </button>
+      )}
       <Link to={`/book/${bookId}`}>Go back to the review's books</Link>
     </div>
   );
