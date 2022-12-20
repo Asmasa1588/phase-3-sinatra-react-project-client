a3a8e05ecc190a8f90ca00d1d890bfb3dad7a16f
Asma <as1588ma@gmail.com>
2022-12-20 12:38:15 -0500

    create a book view for its review

--- src/Book.js
+++ src/Book.js
@@ -1,3 +1,34 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 export const Book = () => {
-  return <div>Book page in progress</div>;
+  const { id } = useParams();
+  const [book, setBook] = useState();
+  useEffect(() => {
+    fetch(`http://localhost:9292/books/${id}`)
+      .then((res) => res.json())
+      .then((receivedBook) => {
+        setBook(receivedBook);
+      });
+  }, [id]);
+  return (
+    <div>
+      {book && (
+        <div>
+          <div>
+            <div>Title: {book.title}</div>
+            <div>Year: {book.year}</div>
+            <div>Author: {book.author}</div>
+            <div>Pages: {book.pages}</div>
+          </div>
+          <div>
+            <h2>Reviews:</h2>
+            {(book.reviews || []).map((review) => {
+              return <p key={review.id}>{review.review}</p>;
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
+
